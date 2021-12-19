61fb91c79fa6f4f2c5f5db861ef6353e5b449b98
Quang <htquangg@gmail.com>
2021-12-20 02:08:31 +0700

    fix: adjust style header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Center, Stack, Text } from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { Box, Stack, Text, useColorModeValue } from 'native-base';
 
 const Header = () => {
   return (
-    <Center>
-      <Stack space="2" height="16" justifyContent="center">
-        <Text fontSize="3xl" fontWeight="bold">
+    <Box px={2}>
+      <Stack flexDirection="row" space="2" height="20" alignItems="center">
+        <Icon name="github" solid size={32} />
+        <Text
+          fontSize="4xl"
+          fontWeight="bold"
+          ml="2"
+          color={useColorModeValue('darkText', 'lightText')}
+        >
           Git hub
         </Text>
       </Stack>
-    </Center>
+    </Box>
   );
 };
 
